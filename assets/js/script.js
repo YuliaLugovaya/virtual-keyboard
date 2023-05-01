be471cccb13ff9db3480dc354141f823458d9247
YuliaLugovaya <yulialugovayaphoto@gmail.com>
2023-05-01 12:21:54 +0300

    feat: add uppercase for capslock and focus for textarea

--- assets/js/script.js
+++ assets/js/script.js
@@ -7,6 +7,11 @@ const rowThird = ['CapsLock', 'a', 's', 'd', 'f', 'g', 'h', 'j', 'k', 'l', ';', 
 const rowFourth = ['Shift', 'z', 'x', 'c', 'v', 'b', 'n', 'm', ',', '.', '/', '▲', 'Shift'];
 const rowFifth = ['Ctrl', 'Win', 'Alt', '', 'Alt', '◀', '▼', '▶', 'Ctrl'];
 
+const rowFirstRus = ['ё', '1', '2', '3', '4', '5', '6', '7', '8', '9', '0', '-', '=', 'Backspace'];
+const rowSecondRus = ['Tab', 'й', 'ц', 'у', 'к', 'е', 'н', 'г', 'ш', 'щ', 'з', 'х', 'ъ', '\\', 'Del'];
+const rowThirdRus = ['CapsLock', 'ф', 'ы', 'в', 'а', 'п', 'р', 'о', 'л', 'д', 'ж', 'э', 'Enter'];
+const rowFourthRus = ['Shift', 'я', 'ч', 'с', 'м', 'и', 'т', 'ь', 'б', 'ю', '.', '▲', 'Shift'];
+
 // ссылка на body
 const body = document.querySelector('body');
 
@@ -27,7 +32,7 @@ const rowFifthContainer = document.createElement('div');
 wrapper.className = 'wrapper';
 title.className = 'title';
 textarea.className = 'textarea';
-container.className = 'container';
+container.className = 'container en';
 textFirst.className = 'text text-first';
 textSecond.className = 'text text-second';
 rowFirstContainer.className = 'row row-first';
@@ -54,13 +59,17 @@ title.innerText = 'Virtual Keyboard';
 textFirst.innerText = 'Клавиатура создана в операционной системе Windows';
 textSecond.innerText = 'Комбинация для переключения языка: левые alt + shift';
 
+// фокус в текстовом поле при нажатии на любую клавишу
+window.addEventListener('keydown', () => {
+  textarea.focus();
+});
+
 // отрисовка кнопок в клавиатуру
 // первый ряд
 function initRowFirst() {
   for (let i = 0; i < rowFirst.length; i++) {
     const button = document.createElement('div');
     button.className = 'button';
-    button.dataset.code = rowFirst[i];
     button.innerText = rowFirst[i];
     button.setAttribute('lowerCase', button.innerText);
     button.setAttribute('upperCase', button.innerText.toUpperCase());
@@ -86,6 +95,35 @@ function initRowFirst() {
   return rowFirstContainer;
 }
 initRowFirst();
+
+function initRowFirstRus() {
+  for (let i = 0; i < rowFirstRus.length; i++) {
+    const button = document.createElement('div');
+    button.className = 'button';
+    button.innerText = rowFirstRus[i];
+    button.setAttribute('lowerCase', button.innerText);
+    button.setAttribute('upperCase', button.innerText.toUpperCase());
+    if (rowFirstRus[i] === 'Backspace') {
+      button.className = 'button backspace';
+    }
+    window.addEventListener('keydown', (e) => {
+      if (e.key === button.getAttribute('lowerCase') || e.key === button.getAttribute('upperCase')) {
+        button.classList.add('active');
+      }
+    });
+    window.addEventListener('keyup', (e) => {
+      if (e.key === button.getAttribute('lowerCase') || e.key === button.getAttribute('upperCase')) {
+        button.classList.remove('active');
+        button.classList.add('remove');
+      }
+      setTimeout(() => {
+        button.classList.remove('remove');
+      });
+    });
+    rowFirstContainer.appendChild(button);
+  }
+  return rowFirstContainer;
+}
 
 // второй ряд
 function initRowSecond() {
@@ -115,11 +153,46 @@ function initRowSecond() {
         button.classList.remove('remove');
       });
     });
+    button.addEventListener('click', (e) => {
+      textarea.innerText = e.code;
+    });
     rowSecondContainer.appendChild(button);
   }
   return rowSecondContainer;
 }
 initRowSecond();
+
+function initRowSecondRus() {
+  for (let i = 0; i < rowSecondRus.length; i++) {
+    const button = document.createElement('div');
+    button.className = 'button';
+    button.innerText = rowSecondRus[i];
+    button.setAttribute('lowerCase', button.innerText);
+    button.setAttribute('upperCase', button.innerText.toUpperCase());
+    if (rowSecondRus[i] === 'Tab') {
+      button.className = 'button tab';
+    }
+    if (rowSecondRus[i] === 'Del') {
+      button.className = 'button delete';
+    }
+    window.addEventListener('keydown', (e) => {
+      if (e.key === button.getAttribute('lowerCase') || e.key === button.getAttribute('upperCase')) {
+        button.classList.add('active');
+      }
+    });
+    window.addEventListener('keyup', (e) => {
+      if (e.key === button.getAttribute('lowerCase') || e.key === button.getAttribute('upperCase')) {
+        button.classList.remove('active');
+        button.classList.add('remove');
+      }
+      setTimeout(() => {
+        button.classList.remove('remove');
+      });
+    });
+    rowSecondContainer.appendChild(button);
+  }
+  return rowSecondContainer;
+}
 
 // третий ряд
 function initRowThird() {
@@ -133,6 +206,44 @@ function initRowThird() {
       button.className = 'button capslock';
     }
     if (rowThird[i] === 'Enter') {
+      button.className = 'button enter';
+    }
+    window.addEventListener('keydown', (e) => {
+      if (e.key === button.getAttribute('lowerCase') || e.key === button.getAttribute('upperCase')) {
+        if (e.key === 'CapsLock') {
+          button.classList.toggle('active');
+          container.classList.toggle('uppercase');
+        } else {
+          button.classList.add('active');
+        }
+      }
+    });
+    window.addEventListener('keyup', (e) => {
+      if ((e.key === button.getAttribute('lowerCase') || e.key === button.getAttribute('upperCase')) && e.key !== 'CapsLock') {
+        button.classList.remove('active');
+        button.classList.add('remove');
+      }
+      setTimeout(() => {
+        button.classList.remove('remove');
+      });
+    });
+    rowThirdContainer.appendChild(button);
+  }
+  return rowThirdContainer;
+}
+initRowThird();
+
+function initRowThirdRus() {
+  for (let i = 0; i < rowThird.length; i++) {
+    const button = document.createElement('div');
+    button.className = 'button';
+    button.innerText = rowThirdRus[i];
+    button.setAttribute('lowerCase', button.innerText);
+    button.setAttribute('upperCase', button.innerText.toUpperCase());
+    if (rowThirdRus[i] === 'CapsLock') {
+      button.className = 'button capslock';
+    }
+    if (rowThirdRus[i] === 'Enter') {
       button.className = 'button enter';
     }
     window.addEventListener('keydown', (e) => {
@@ -162,7 +273,6 @@ function initRowThird() {
   }
   return rowThirdContainer;
 }
-initRowThird();
 
 // четвертый ряд
 function initRowFourth() {
@@ -201,6 +311,41 @@ function initRowFourth() {
 }
 initRowFourth();
 
+function initRowFourthRus() {
+  for (let i = 0; i < rowFourthRus.length; i++) {
+    const button = document.createElement('div');
+    button.className = 'button';
+    button.innerText = rowFourthRus[i];
+    button.setAttribute('lowerCase', button.innerText);
+    button.setAttribute('upperCase', button.innerText.toUpperCase());
+    if (rowFourthRus[i] === 'Shift' && i === 0) {
+      button.className = 'button shift-left';
+    }
+    if (rowFourthRus[i] === 'Shift' && i === rowFourthRus.length - 1) {
+      button.className = 'button shift-right';
+    }
+    if (rowFourthRus[i] === '▲') {
+      button.className = 'button top';
+    }
+    window.addEventListener('keydown', (e) => {
+      if (e.key === button.getAttribute('lowerCase') || e.key === button.getAttribute('upperCase')) {
+        button.classList.add('active');
+      }
+    });
+    window.addEventListener('keyup', (e) => {
+      if (e.key === button.getAttribute('lowerCase') || e.key === button.getAttribute('upperCase')) {
+        button.classList.remove('active');
+        button.classList.add('remove');
+      }
+      setTimeout(() => {
+        button.classList.remove('remove');
+      });
+    });
+    rowFourthContainer.appendChild(button);
+  }
+  return rowFourthContainer;
+}
+
 // пятый ряд
 function initRowFifth() {
   for (let i = 0; i < rowFifth.length; i++) {
@@ -212,7 +357,7 @@ function initRowFifth() {
     if (rowFifth[i] === 'Ctrl' && i === 0) {
       button.className = 'button ctrl-left';
     }
-    if (rowFifth[i] === 'Ctrl' && rowFifth.length - 1) {
+    if (rowFifth[i] === 'Ctrl' && i !== 0) {
       button.className = 'button ctrl-right';
     }
     if (rowFifth[i] === 'Alt' && i === 2) {
@@ -255,3 +400,31 @@ function initRowFifth() {
   return rowFifthContainer;
 }
 initRowFifth();
+
+// window.addEventListener('keydown', (e) => {
+//   if (e.key === '1') {
+//     container.classList.toggle('rus');
+//     container.classList.remove('en');
+//   }
+//   if (container.classList.contains('rus')) {
+//     rowFirstContainer.replaceWith('1');
+//     rowSecondContainer.replaceWith('2');
+//     rowThirdContainer.replaceWith('3');
+//     rowFourthContainer.replaceWith('4');
+//     initRowFirstRus();
+//     initRowSecondRus();
+//     initRowThirdRus();
+//     initRowFourthRus();
+//   } else if (!container.classList.contains('rus')) {
+//     container.classList.add('en');
+//   } else if (container.classList.contains('en')) {
+//     rowFirstContainer.replaceWith('1');
+//     rowSecondContainer.replaceWith('2');
+//     rowThirdContainer.replaceWith('3');
+//     rowFourthContainer.replaceWith('');
+//     initRowFirst();
+//     initRowSecond();
+//     initRowThird();
+//     initRowFourth();
+//   }
+// });
